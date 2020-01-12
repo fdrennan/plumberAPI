028e0fb2e245082f6925e4bc99c2a4f7212baa86
Freddy Drennan <drennanfreddy@gmail.com>
2020-01-12 08:42:49 -0700

    update

--- misc/plumber.R
+++ misc/plumber.R
@@ -1,24 +1,42 @@
-#
-# This is a Plumber API. You can run the API by clicking
-# the 'Run API' button above.
-#
-# Find out more about building APIs with Plumber here:
-#
-#    https://www.rplumber.io/
-#
+#* @Plumber Example
 
 library(plumber)
 library(plumberAPI)
 
-#* @apiTitle Plumber Example API
+#' @filter cors
+cors <- function(req, res) {
 
+  res$setHeader("Access-Control-Allow-Origin", "*")
 
+  if (req$REQUEST_METHOD == "OPTIONS") {
+    res$setHeader("Access-Control-Allow-Methods","*")
+    res$setHeader("Access-Control-Allow-Headers", req$HTTP_ACCESS_CONTROL_REQUEST_HEADERS)
+    res$status <- 200
+    return(list())
+  } else {
+    plumber::forward()
+  }
+
+}
 
 #* Echo back the input
 #* @param msg The message to echo
-#* @get /wait
-function() {
+#* @get /save_cars
+save_cars <- function() {
   save_cars()
   list(msg = paste0("Completed save cars"))
 }
 
+#* Echo back the input
+#* @param time The time needed to wait
+#* @get /wait
+wait <- function(time = 5) {
+  Sys.sleep(time)
+  list(msg = glue("Completed wait for {time}"))
+}
+
+#* Echo back the input
+#* @get /data
+data <- function() {
+  toJSON(mtcars)
+}
